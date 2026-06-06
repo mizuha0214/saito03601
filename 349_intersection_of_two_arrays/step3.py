ac8fb45c1a874da4cd9b3c3e8b6f17b5baa5f570
mizuha Saito <0214mizuha@gmail.com>
2026-06-07 06:48:25 +0900

    forgot add files

--- 349_intersection_of_two_arrays/step3.py
+++ 349_intersection_of_two_arrays/step3.py
@@ -5,9 +5,9 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         intersection_nums = set()
 
-        num1_set = set(nums1) # O(len(nums1))
-        for num2 in nums2:  # O(len(nums2))
-            if num2 in num1_set:  # O(1)
+        nums1_set = set(nums1)
+        for num2 in nums2:
+            if num2 in nums1_set:
                 intersection_nums.add(num2)
 
         return list(intersection_nums)
